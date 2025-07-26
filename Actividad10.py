eb17660719b183152346f91ba647733ea113c6b7
Rudy <rudyax2004@gmail.com>
2025-07-26 14:30:19 -0600

    Segundo Commit

--- Actividad10.py
+++ Actividad10.py
@@ -7,7 +7,7 @@ print("Bienvenidos Actividad 10")
 while(opcion != 6):
     print("Bienvenidos menu iteracción")
     print("1.- Invertir una cadena de texto")
-    print("2.- Calcular la suma de dos numeros ")
+    print("2.- Calcular la suma de los  numeros ")
     print("3.- Imprimir una cuenta regresiva desde N números naturales hasta 1 ")
     print("4.- Sumar los digitos de un número")
     print("5.- Contar cuantos díjitos tiene un número")
@@ -19,6 +19,9 @@ while(opcion != 6):
             print("Invertir Una cadena de texto")
             palabra = input("Ingrese la palabra que desea invertir: ")
             print(invertir(palabra))
+        case 2:
+            print("\n Bienvenido a escogido la opcion 2")
+            print("Calcular la suma de dos numeros ")
 
 
 
