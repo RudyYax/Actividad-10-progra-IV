fb6477a6d6658dab8e9c7e0c35469d3f2dcb76fb
Rudy <rudyax2004@gmail.com>
2025-07-26 14:20:43 -0600

    Segundo Commit

--- Actividad10.py
+++ Actividad10.py
@@ -1,1 +1,11 @@
+opcion = 0
 print("Bienvenidos Actividad 10")
+while(opcion != 6):
+    print("Bienvenidos menu iteracción")
+    print("1.- Invertir una cadena de texto")
+    print("2.- Calcular la suma de dos numeros ")
+    print("3.- Imprimir una cuenta regresiva desde N números naturales hasta 1 ")
+    print("4.- Sumar los digitos de un número")
+    print("5.- Contar cuantos díjitos tiene un número")
+    print("6.- Salir")
+
