ee4528ab2136adaa9775f1be37cca4a3068f4932
Rudy <rudyax2004@gmail.com>
2025-07-26 14:26:44 -0600

    Segundo Commit

--- Actividad10.py
+++ Actividad10.py
@@ -1,3 +1,7 @@
+def invertir(palabra):
+    return palabra[::-1]
+print(invertir('Palabra'))
+
 opcion = 0
 print("Bienvenidos Actividad 10")
 while(opcion != 6):
@@ -8,4 +12,13 @@ while(opcion != 6):
     print("4.- Sumar los digitos de un número")
     print("5.- Contar cuantos díjitos tiene un número")
     print("6.- Salir")
+    opcion = int(input("Ingrese la opcion que desee: "))
+    match(opcion):
+        case 1:
+            print("\n Bienvenido a escogido la opcion 1")
+            print("Invertir Una cadena de texto")
+            palabra = input("Ingrese la palabra que desea invertir: ")
+            print(invertir(palabra))
+
+
 
